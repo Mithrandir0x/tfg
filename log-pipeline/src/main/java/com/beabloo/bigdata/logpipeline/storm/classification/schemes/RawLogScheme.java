139e3dcfa5acaf4d5821175c9aaa655891b66fdc
Oriol López Sánchez <mithrandir0x@gmail.com>
2016-05-16 16:36:29 +0200

    Restored topology to see if current changes do work.

--- log-pipeline/src/main/java/com/beabloo/bigdata/logpipeline/storm/classification/schemes/RawLogScheme.java
+++ log-pipeline/src/main/java/com/beabloo/bigdata/logpipeline/storm/classification/schemes/RawLogScheme.java
@@ -7,33 +7,22 @@ import com.beabloo.bigdata.loggateway.integrations.kafka.serdes.RawLogSerDe;
 import com.beabloo.bigdata.model.RawLog;
 
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RawLogScheme implements Scheme {
 
-//    @Override
-//    public List<Object> deserialize(byte[] ser) {
-//        RawLogSerDe serDe = new RawLogSerDe();
-//        RawLog rawLog = serDe.deserialize(null, ser);
-//        return new Values(rawLog.getTimestamp(), rawLog.getType(), rawLog.getData());
-//    }
-
     private static final Logger log = LoggerFactory.getLogger(RawLogScheme.class);
 
     @Override
     public List<Object> deserialize(ByteBuffer payload) {
-        RawLogSerDe serDe = new RawLogSerDe();
-        //byte[] bufferBytes = buffer.array();
-        //byte[] bytes = Arrays.copyOfRange(bufferBytes, 71, bufferBytes.length);
         byte[] bytes = new byte[payload.limit()];
         payload.get(bytes);
 
-        //log.info(String.format("bufferBytes [%s]", bytesToHex(bufferBytes)));
-        log.info(String.format("bytes [%s]", bytesToHex(bytes)));
+//        log.debug(String.format("bytes [%s]", bytesToHex(bytes)));
 
+        RawLogSerDe serDe = new RawLogSerDe();
         RawLog rawLog = serDe.deserialize(null, bytes);
 
         return new Values(rawLog.getTimestamp(), rawLog.getType(), rawLog.getData());
@@ -44,21 +33,21 @@ public class RawLogScheme implements Scheme {
         return new Fields("timestamp", "type", "data");
     }
 
-    final private static char[] hexArray = "0123456789ABCDEF".toCharArray();
-    private static String bytesToHex(byte[] bytes) {
-        StringBuffer buffer = new StringBuffer();
-        char l, r;
-        for ( int j = 0; j < bytes.length; j++ ) {
-            int v = bytes[j] & 0xFF;
-            l = hexArray[v >>> 4];
-            r = hexArray[v & 0x0F];
-            if ( j != bytes.length - 1 ) {
-                buffer.append(String.format("%c%c ", l, r));
-            } else {
-                buffer.append(String.format("%c%c", l, r));
-            }
-        }
-        return buffer.toString();
-    }
+//    final private static char[] hexArray = "0123456789ABCDEF".toCharArray();
+//    private static String bytesToHex(byte[] bytes) {
+//        StringBuffer buffer = new StringBuffer();
+//        char l, r;
+//        for ( int j = 0; j < bytes.length; j++ ) {
+//            int v = bytes[j] & 0xFF;
+//            l = hexArray[v >>> 4];
+//            r = hexArray[v & 0x0F];
+//            if ( j != bytes.length - 1 ) {
+//                buffer.append(String.format("%c%c ", l, r));
+//            } else {
+//                buffer.append(String.format("%c%c", l, r));
+//            }
+//        }
+//        return buffer.toString();
+//    }
 
 }
--- log-pipeline/src/main/java/com/beabloo/bigdata/logpipeline/storm/classification/topologies/LogClassificationTopology.java
+++ log-pipeline/src/main/java/com/beabloo/bigdata/logpipeline/storm/classification/topologies/LogClassificationTopology.java
@@ -4,7 +4,6 @@ import com.beabloo.bigdata.logpipeline.storm.classification.bolts.CockroachModel
 import com.beabloo.bigdata.logpipeline.storm.classification.bolts.CockroachUnpackerBolt;
 import com.beabloo.bigdata.logpipeline.storm.classification.bolts.RawLogProtocolSplitterBolt;
 import com.beabloo.bigdata.logpipeline.storm.classification.bolts.hdfs.HdfsLogSelector;
-import com.beabloo.bigdata.logpipeline.storm.classification.metrics.PrometheusMetricsConsumer;
 import com.beabloo.bigdata.logpipeline.storm.classification.spouts.RawLogKafkaSpout;
 import org.apache.storm.Config;
 import org.apache.storm.StormSubmitter;
@@ -15,7 +14,6 @@ import org.apache.storm.hdfs.bolt.rotation.FileRotationPolicy;
 import org.apache.storm.hdfs.bolt.rotation.FileSizeRotationPolicy;
 import org.apache.storm.hdfs.bolt.sync.CountSyncPolicy;
 import org.apache.storm.hdfs.bolt.sync.SyncPolicy;
-import org.apache.storm.metric.LoggingMetricsConsumer;
 import org.apache.storm.topology.TopologyBuilder;
 
 public class LogClassificationTopology {
@@ -37,34 +35,34 @@ public class LogClassificationTopology {
             RawLogProtocolSplitterBolt rawLogProtocolSplitterBolt = new RawLogProtocolSplitterBolt();
             builder.setBolt(RawLogProtocolSplitterBolt.ID, rawLogProtocolSplitterBolt)
                     .shuffleGrouping(RawLogKafkaSpout.ID);
-//
-//            CockroachUnpackerBolt cockroachUnpackerBolt = new CockroachUnpackerBolt();
-//            builder.setBolt(CockroachUnpackerBolt.ID, cockroachUnpackerBolt)
-//                    .shuffleGrouping(RawLogProtocolSplitterBolt.ID, RawLogProtocolSplitterBolt.HTTP_COCKROACH_STREAM);
-//
-//            CockroachModelParserBolt cockroachModelParserBolt = new CockroachModelParserBolt();
-//            builder.setBolt(CockroachModelParserBolt.ID, cockroachModelParserBolt)
-//                    .shuffleGrouping(CockroachUnpackerBolt.ID);
-//
-//            // sync the filesystem after every 1k tuples
-//            SyncPolicy syncPolicy = new CountSyncPolicy(10);
-//
-//            // rotate files when they reach 5MB
-//            FileRotationPolicy rotationPolicy = new FileSizeRotationPolicy(2.0f, FileSizeRotationPolicy.Units.MB);
-//            //TimedRotationPolicy rotationPolicy = new TimedRotationPolicy(5, TimedRotationPolicy.TimeUnit.SECONDS);
-//
-//            FileNameFormat fileNameFormat = new DefaultFileNameFormat().withPath("/engine2_big_data/cockroach/");
-//
-//            HdfsBolt hdfsBolt = new HdfsBolt()
-//                    // @TODO The NameNode server should be parameterized
-//                    .withFsUrl("hdfs://nn.local.vm:8020")
-//                    .withFileNameFormat(fileNameFormat)
-//                    .withRecordFormat(HdfsLogSelector.getHdfsLogFormat())
-//                    .withPartitioner(HdfsLogSelector.getHdfsLogPartitioner())
-//                    .withRotationPolicy(rotationPolicy)
-//                    .withSyncPolicy(syncPolicy);
-//            builder.setBolt("COCKROACH_HDFS_BOLT_ID", hdfsBolt)
-//                    .shuffleGrouping(CockroachModelParserBolt.ID);
+
+            CockroachUnpackerBolt cockroachUnpackerBolt = new CockroachUnpackerBolt();
+            builder.setBolt(CockroachUnpackerBolt.ID, cockroachUnpackerBolt)
+                    .shuffleGrouping(RawLogProtocolSplitterBolt.ID, RawLogProtocolSplitterBolt.HTTP_COCKROACH_STREAM);
+
+            CockroachModelParserBolt cockroachModelParserBolt = new CockroachModelParserBolt();
+            builder.setBolt(CockroachModelParserBolt.ID, cockroachModelParserBolt)
+                    .shuffleGrouping(CockroachUnpackerBolt.ID);
+
+            // sync the filesystem after every 1k tuples
+            SyncPolicy syncPolicy = new CountSyncPolicy(10);
+
+            // rotate files when they reach 5MB
+            FileRotationPolicy rotationPolicy = new FileSizeRotationPolicy(2.0f, FileSizeRotationPolicy.Units.MB);
+            //TimedRotationPolicy rotationPolicy = new TimedRotationPolicy(5, TimedRotationPolicy.TimeUnit.SECONDS);
+
+            FileNameFormat fileNameFormat = new DefaultFileNameFormat().withPath("/engine2_big_data/cockroach/");
+
+            HdfsBolt hdfsBolt = new HdfsBolt()
+                    // @TODO The NameNode server should be parameterized
+                    .withFsUrl("hdfs://nn.local.vm:8020")
+                    .withFileNameFormat(fileNameFormat)
+                    .withRecordFormat(HdfsLogSelector.getHdfsLogFormat())
+                    .withPartitioner(HdfsLogSelector.getHdfsLogPartitioner())
+                    .withRotationPolicy(rotationPolicy)
+                    .withSyncPolicy(syncPolicy);
+            builder.setBolt("COCKROACH_HDFS_BOLT_ID", hdfsBolt)
+                    .shuffleGrouping(CockroachModelParserBolt.ID);
 
             config.setNumWorkers(4);
 
