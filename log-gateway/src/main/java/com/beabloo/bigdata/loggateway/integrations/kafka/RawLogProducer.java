63d2f8fe4fa1b73f6bd07b6ce9e6c3a3a5911e70
Oriol López Sánchez <mithrandir0x@gmail.com>
2016-05-16 14:01:58 +0200

    Changed kafka producer's default partitioner to a long value based one.

--- log-gateway/src/main/java/com/beabloo/bigdata/loggateway/integrations/kafka/RawLogProducer.java
+++ log-gateway/src/main/java/com/beabloo/bigdata/loggateway/integrations/kafka/RawLogProducer.java
@@ -3,7 +3,7 @@ package com.beabloo.bigdata.loggateway.integrations.kafka;
 import com.beabloo.bigdata.loggateway.integrations.kafka.serdes.RawLogSerDe;
 import com.beabloo.bigdata.model.RawLog;
 import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.LongSerializer;
 
 import java.util.Properties;
 
@@ -22,13 +22,14 @@ public class RawLogProducer {
             String kafkaVersion = System.getProperty("kafka.version"); // -Dkafka.version=0.9
             if ( "0.9".equals(kafkaVersion) ) {
                 properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getProperty("log_gateway.kafka.producer.bootstrap.servers")); // -Dlog_gateway.kafka.producer.bootstrap.servers=localhost:9092
-                properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+                properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
                 properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, RawLogSerDe.class.getName());
                 properties.setProperty(ProducerConfig.PARTITIONER_CLASS_CONFIG, LongValuePartitioner.class.getName());
             } else if ( "0.8.2.2".equals(kafkaVersion) ) {
                 properties.setProperty("metadata.broker.list", System.getProperty("log_gateway.kafka.metadata.broker.list")); // -Dlog_gateway.kafka.metadata.broker.list=localhost:9092
-                properties.setProperty("key.serializer.class", StringSerializer.class.getName());
+                properties.setProperty("key.serializer.class", LongSerializer.class.getName());
                 properties.setProperty("serializer.class", RawLogSerDe.class.getName());
+                properties.setProperty("partitioner.class", LongValuePartitioner.class.getName());
             }
             properties.setProperty(ProducerConfig.CLIENT_ID_CONFIG, System.getProperty("log_gateway.kafka.producer.client.id")); // -Dlog_gateway.kafka.producer.client.id=local
             properties.setProperty(ProducerConfig.RECONNECT_BACKOFF_MS_CONFIG, "60000");
