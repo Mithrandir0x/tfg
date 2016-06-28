fce6bd634543ea268dd8e8ed97263879baedfab3
Oriol López Sánchez <mithrandir0x@gmail.com>
2016-06-28 10:35:41 +0200

    Fixed another stoopid typo.

--- structor/modules/grafana/manifests/init.pp
+++ structor/modules/grafana/manifests/init.pp
@@ -13,7 +13,7 @@ class grafana {
     }
     ->
     exec { "install-grafana":
-        command => "yum -y install grafana-$VERSION.linux-x64.rpm",
+        command => "yum -y install ./grafana-$VERSION.linux-x64.rpm",
         cwd => "/tmp",
         path => $PATH,
     }
