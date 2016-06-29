7f41e58a75cce47536f62b16e554a5316bae6190
Oriol López Sánchez <mithrandir0x@gmail.com>
2016-06-29 03:34:02 +0200

    Changed code to see why interceptors stopped working.

--- log-gateway/src/main/java/edu/ub/bigdata/loggateway/aspects/integration/KafkaRawLogProducerInterceptor.java
+++ log-gateway/src/main/java/edu/ub/bigdata/loggateway/aspects/integration/KafkaRawLogProducerInterceptor.java
@@ -9,7 +9,7 @@ import org.aspectj.lang.annotation.Aspect;
 @Aspect
 public class KafkaRawLogProducerInterceptor {
 
-    @Around("execution(* (@edu.ub.bigdata.loggateway.aspects.integration.KafkaRawLogProducer edu.ub.bigdata.loggateway.LogListener<INPUT, OUTPUT>+).onStoreLogObject(..))")
+    @Around("execution(* (@edu.ub.bigdata.loggateway.aspects.integration.KafkaRawLogProducer edu.ub.bigdata.loggateway.LogListener+).onStoreLogObject(..))")
     public void sendToKafka(ProceedingJoinPoint pjp) throws Throwable {
         pjp.proceed();
 
